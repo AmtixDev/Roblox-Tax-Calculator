6c3c970ae48054d3ccf7d97e8028d6b1fc9e1795
AmtiX <107303548+AmtixDev@users.noreply.github.com>
2024-03-22 21:35:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ function calculateRobloxTax(amount) {
     const taxPercentage = 30;
     const taxAmount = (amount * taxPercentage) / 100;
     const netAmount = amount - taxAmount;
+    const remittanceAmount = taxAmount;
     return {
         grossAmount: amount,
         taxAmount: taxAmount,
-        netAmount: netAmount
+        netAmount: netAmount,
+        remittanceAmount: remittanceAmount
     };
 }
 
